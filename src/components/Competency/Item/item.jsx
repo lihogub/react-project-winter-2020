d02d6eeb32c95cab9e5aabbd229b6f417c833a3e
v.shustov <57774085+shuimi@users.noreply.github.com>
2020-11-30 11:44:04 +0300

    images added

--- src/components/Competency/Item/item.jsx
+++ src/components/Competency/Item/item.jsx
@@ -2,16 +2,30 @@ import React from "react";
 
 export default class CompetencyItem extends React.Component {
 
-    styleObj = {
-        backgroundColor: 'red'
+    imgStyle = {
+        backgroundColor: 'red',
+        maxHeight: 81,
+        maxWidth: 81
+    }
+
+    backgroundImgStyle = {
+        backgroundColor: 'blue',
+        opacity: 0.3,
+        maxHeight: 81,
+        maxWidth: 81
     }
 
     render() {
         return (
             <div className=''>
-                <div style = {this.styleObj}>
+                <div style = {this.imgStyle}>
                     <img src={this.props.imageUrl}/>
                 </div>
+
+                <div style = {this.backgroundImgStyle}>
+                    <img src={this.props.imageUrl}/>
+                </div>
+
                 <div>
                     {this.props.text}
                 </div>
