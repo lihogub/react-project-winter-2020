e6410c1d93754bf1e643df5169c52238a4d7a17b
v.shustov <57774085+shuimi@users.noreply.github.com>
2020-11-30 20:39:17 +0300

    app.js merged & fixed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ export default class App extends React.Component {
             <Container>
                 <Header/>
                 <Competency/>
-                <Workers />git
+                <Workers />
             </Container>
         )
     }
