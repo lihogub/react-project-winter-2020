8a914bc6be9b068590c79d92bc631cb9910a2bf2
Oleg Lihogub <oligli@yandex.ru>
2020-12-02 09:26:26 +0300

    warnings fixed

--- src/components/Competency/Item/item.jsx
+++ src/components/Competency/Item/item.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import BGImg from "./../../../res/images/drupal-coder-bg.png";
 import styles from "./item.module.css";
-import {Card} from "react-bootstrap";
 
 export default class CompetencyItem extends React.Component {
 
@@ -9,8 +8,8 @@ export default class CompetencyItem extends React.Component {
         return (
             <div className={styles.common}>
                 <div className=''>
-                    <img className={styles.icon} src={this.props.imageUrl}/>
-                    <img className={styles.bg_image} src={BGImg}/>
+                    <img className={styles.icon} src={this.props.imageUrl} alt="competency-icon"/>
+                    <img className={styles.bg_image} src={BGImg} alt="drupal-overlay-icon"/>
                 </div>
                 <div className='m-3'>
                     {this.props.text}
