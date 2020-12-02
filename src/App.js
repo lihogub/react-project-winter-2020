871fa4b681bd45984de1f37b65947478d925b42e
m.bespalov <max.bespalov23@gmail.com>
2020-12-02 13:17:21 +0300

    base of footer

--- src/App.js
+++ src/App.js
@@ -5,16 +5,20 @@ import {Container} from "react-bootstrap";
 import Competency from "./components/Competency/Competency";
 import Workers from "./components/Workers/Workers";
 import Advantage from "./components/Advantage/Advantage";
+import Foottter from "./components/Foottter/Foottter";
 
 export default class App extends React.Component {
     render() {
         return (
-            <Container fluid={true}>
-                <Header/>
-                <Advantage/>
-                <Workers/>
-                <Competency/>
-            </Container>
+            <div>
+                <Container fluid={true}>
+                    {/*<Header/>*/}
+                    <Advantage/>
+                    {/*<Workers/>*/}
+                    <Competency/>
+                </Container>
+                <Foottter/>
+            </div>
         )
     }
 }
---
+++ src/components/Foottter/Foottter.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import styles from "./Foottter.module.css"
+import drupalCoderImg from "../../res/images/drupal-coder-bg.png"
+
+export default class Foottter extends React.Component {
+
+    render() {
+        return (
+            <footer className={styles.footerGlobal}>
+                <div className={styles.monoLogo}>
+                    <img src={drupalCoderImg}/>
+                </div>
+                <div>
+                    <a href="https://facebook.com">
+                        <img className={styles.socialNetworkImg}
+                             src="https://cdn.worldvectorlogo.com/logos/facebook-icon-1.svg"/>
+                    </a>
+                    <a href="https://vk.com">
+                        <img className={styles.socialNetworkImg}
+                             src="https://lh3.googleusercontent.com/proxy/HyryrN46dNlqJ8jjh90JyG_URRiUdgjBwHVK57G7UjtGoFe1Qc8w2gzHHXi3ORR0FhhndBHQJA54VkY9su050l5i9yuj1GK8l9bAgvCc_5P0kSGdVdvEMLpmNYgwiMExapQ"/>
+                    </a>
+                    <a href="https://tlgrm.ru/">
+                        <img className={styles.socialNetworkImg}
+                             src="https://i.pinimg.com/originals/1c/39/c8/1c39c81fccba10c48869ce903c533845.png"/>
+                    </a>
+                    <a href="https://facebook.com">
+                        <img className={styles.socialNetworkImg}
+                             src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/bd/YouTube_social_dark_squircle_%282017%29.svg/1024px-YouTube_social_dark_squircle_%282017%29.svg.png"/>
+                    </a>
+                </div>
+                <div className={styles.copyRightAndLogo}>
+                    <p className={styles.copyRight}>
+                        Проект ООО «Инитлаб», Краснодар, Россия.<br/>
+                        Drupal является зарегистрированной торговой маркой Dries Buytaert.
+                    </p>
+                    <img src={drupalCoderImg} className={styles.botLogo}/>
+                </div>
+            </footer>
+        )
+    }
+}
