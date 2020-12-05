88289a5929209c7a292f40a0e37c875e596052f3
Oleg Lihogub <oligli@yandex.ru>
2020-12-05 17:37:04 +0300

    work

---
+++ src/components/Header/Achievements/Achievements.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import axios from "axios";
+import AchievementItem from "./Item/AchievementItem";
+const endpoint = "https://react-warriors-rest-api.herokuapp.com/api/achievement";
+
+export default class Achievements extends React.Component {
+
+    state = {
+        jsonData: []
+    }
+
+    componentDidMount() {
+        axios.get(endpoint).then(
+            (res) => this.setState({jsonData: res.data}),
+            (err) => alert(err)
+        )
+    }
+
+    render() {
+        return (
+            <div className="row d-flex px-0">
+                {
+                    this.state.jsonData.map(
+                        (item) => <AchievementItem title={item.title} text={item.desc} key={item.id} isLarge={item.id === 0}/>
+                    )
+                }
+            </div>
+        )
+    }
+
+}
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import headerBGImg from "../../res/images/headerBG.png";
-import {Container} from "react-bootstrap";
 import Menu from "./Menu/Menu";
+import Achievements from "./Achievements/Achievements";
+import styles from "./Header.module.css";
 
 
 export default class Header extends React.Component {
@@ -13,9 +14,24 @@ export default class Header extends React.Component {
 
     render() {
         return (
-            <Container fluid style={this.style} className="px-0 px-md-1">
+            <div style={this.style} className="container-fluid px-0 px-md-1">
                 <Menu/>
-            </Container>
+                <div className="container py-5">
+                    <div className="row">
+                        <div className="col-12 col-lg-6">
+                            <h1 className={styles.headerTitle}>
+                                Поддержка <br/>сайтов на Drupal
+                            </h1>
+                            <h5 className={styles.headerText}>
+                                Сопровождение и поддержка сайтов <br/>на CMS Drupal любых версий и запущенности
+                            </h5>
+                        </div>
+                        <div className="col-12 col-lg-6">
+                            <Achievements/>
+                        </div>
+                    </div>
+                </div>
+            </div>
         )
     }
 
