44446edc838674cbb7ac6c64e92f004931bb7f31
Oleg Lihogub <oligli@yandex.ru>
2020-12-15 23:52:19 +0300

    works with rest api. fix responsive height!

--- src/components/Testimonial/Item/TestimonialItem.jsx
+++ src/components/Testimonial/Item/TestimonialItem.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-const clientImageLink = `${process.env.PUBLIC_URL}/images/logo-0.png`
-
-const clientText = "Долгие поиски единственного и неповторимого мастера на многострадальный сайт www.cielparfum.com, который был собран крайне некомпетентным программистом и раз в месяц стабильно грозил погибнуть, привели меня на сайт и, в итоге, к ребятам из Drupal-coder. И вот уже практически полгода как не проходит и дня, чтобы я не поудивлялась и не порадовалась своему везению! Починили все, что не работало - от поиска до отображения меню. Провели редизайн - не отходя от желаемого, но со своими существенными и качественными дополнениями. Осуществили ряд проектов - конкурсы, тесты и тд. А уж мелких починок и доработок - не счесть! И главное - все качественно и быстро (не взирая на не самый \"быстрый\" тариф). Есть вопросы - замечательный Алексей всегда подскажет, поддержит, отремонтирует и/или просто сделает с нуля. Есть задумка для реализации - замечательный Сергей обсудит и предложит идеальный вариант. Есть проблема - замечательные Надежда и Роман починят, поправят, сделают! Ребята доказали, что эта CMS - мощная и грамотная система управления. Надеюсь, что наше сотрудничество затянется надолго! Спасибо!!!"
-
-const clientSign = "С уважением, Наталья Сушкова руководитель Отдела веб-проектов Группы компаний «Си Эль парфюм»"
-
 export default class TestimonialItem extends React.Component {
 
     textStyle = {
@@ -27,14 +21,15 @@ export default class TestimonialItem extends React.Component {
     }
 
     render() {
+        const imgUrl = `${process.env.PUBLIC_URL}/images/logo-${this.props.id}.png`
         return (
             <div className="px-2 py-2">
-                <img src={clientImageLink} className="py-3"/>
+                <img src={imgUrl} className="py-3"/>
                 <p style={this.textStyle} className="pt-3 pb-1">
-                    {`id ${this.props.id} ${clientText}`}
+                    {this.props.text}
                 </p>
                 <p style={this.signStyle}>
-                    {clientSign}
+                    {this.props.sign}
                     {this.props.link && <a href={this.props.link} style={this.linkStyle}> {this.props.link}</a>}
                 </p>
             </div>
--- src/components/Testimonial/Testimonial.jsx
+++ src/components/Testimonial/Testimonial.jsx
@@ -1,27 +1,48 @@
 import React from "react";
 import Slider from "react-slick";
+import axios from "axios"
 import TestimonialItem from "./Item/TestimonialItem";
 import styles from "./Testimonial.module.css"
 import arrowPng from "../../res/images/arrow.png"
+
+const endpoint = "https://react-warriors-rest-api.herokuapp.com/api/testimonial"
 
 export default class Testimonial extends React.Component {
 
     state = {
         currentSlide: 0,
-        slides: [
-            {id: 0, link: "http://www.cielparfum.com/"},
-            {id: 1, link: "http://www.cielparfum.com/"},
-            {id: 2, link: "http://www.cielparfum.com/"}
-        ]
+        slides: []
+    }
+
+    componentDidMount() {
+        axios.get(endpoint)
+            .then(
+                (res) => {
+                    this.setState({slides: res.data})
+                }
+            )
+            .catch(
+                (err) => alert("Network error " + err)
+            )
+    }
+
+    arrowStyle(props) {
+        return (
+            <div className="d-none"/>
+        );
     }
 
     settings = {
+        dots: false,
+
         infinite: true,
         fade: true,
         speed: 1000,
         slidesToShow: 1,
         slidesToScroll: 1,
-        beforeChange: (current, next) => this.setState({currentSlide: next})
+        beforeChange: (current, next) => this.setState({currentSlide: next}),
+        nextArrow: <this.arrowStyle/>,
+        prevArrow: <this.arrowStyle/>,
     }
 
     padWithZeros(text, targetLength) {
@@ -33,10 +54,10 @@ export default class Testimonial extends React.Component {
 
     render() {
         const slides = this.state.slides.length && this.state.slides.map(
-            (item) => <TestimonialItem id={item.id} link={item.link}/>)
+            (item) => <TestimonialItem id={item.id} link={item.link} text={item.text} sign={item.sign}/>)
 
         return (
-            <div className={`container-fluid container-sm px-0 px-md-5 ${!this.state.slides.length && "d-none"}`}>
+            <div className={`container-fluid container-sm px-0 px-md-5 mb-5 pb-5 ${!this.state.slides.length && "d-none"}`}>
                 <h1 className={`${styles.testimonialTitle} text-center mb-4 mb-md-5`}>
                     Отзывы
                 </h1>
@@ -49,11 +70,13 @@ export default class Testimonial extends React.Component {
                         </div>
                         <div className="col-12 col-xl-4 px-0 d-flex align-items-center justify-content-center">
                             <div className="container-fluid row justify-content-center">
-                                <div className="col-auto row d-flex justify-content-center">
-                                    <img src={arrowPng} className={`${styles.arrow} ${styles.arrowLeft}`}
-                                         onClick={() => this.slider.slickPrev()}/>
+                                <div className="col-auto row d-flex justify-content-center" onClick={(event) => {
+                                    event.preventDefault();
+                                    this.slider.slickPrev();
+                                }}>
+                                    <img src={arrowPng} className={`${styles.arrow} ${styles.arrowLeft}`}/>
                                 </div>
-                                <div className="col row d-flex justify-content-center">
+                                <div className="col-5 col-md-6 row d-flex justify-content-center">
                                     <div className={`${styles.slideCounter} d-flex`}>
                                         <p className={styles.currentSlide}>
                                             {this.padWithZeros(this.state.currentSlide + 1, 2)}
@@ -66,9 +89,11 @@ export default class Testimonial extends React.Component {
                                         </p>
                                     </div>
                                 </div>
-                                <div className="col-auto row d-flex justify-content-center">
-                                    <img src={arrowPng} className={`${styles.arrow} ${styles.arrowRight}`}
-                                         onClick={() => this.slider.slickNext()}/>
+                                <div className="col-auto row d-flex justify-content-center" onClick={(event) => {
+                                    event.preventDefault();
+                                    this.slider.slickNext();
+                                }}>
+                                    <img src={arrowPng} className={`${styles.arrow} ${styles.arrowRight}`}/>
                                 </div>
                             </div>
                         </div>
