937d0be5b351a0075787cc557116828eeab6659b
Oleg Lihogub <oligli@yandex.ru>
2020-12-26 17:39:12 +0300

    added redux-persist

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,8 +1,19 @@
 import {applyMiddleware, compose, createStore} from "redux";
 import {rootReducer} from "../reducers/rootReducer.js"
 import thunk from "redux-thunk";
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const persistConfig = {
+    key: 'root',
+    storage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const store = createStore(persistedReducer, composeEnhancers(applyMiddleware(thunk)));
+persistStore(store)
+
 export {store}
